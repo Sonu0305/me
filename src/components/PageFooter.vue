6df627608eadd0e2403dc4791ba8809e59a5e7a4
Swayam Shah <swayamshah66@gmail.com>
2025-11-09 00:56:30 +0530

    22

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -5,7 +5,6 @@
       <a href="mailto:swayamshah66@gmail.com" target="_blank">swayamshah66@gmail.com </a> |
       <a href="https://www.linkedin.com/in/swayamshahh/" target="_blank">
         <FontAwesomeIcon :icon="['fab', 'square-linkedin']" />
-        swayamshahh
       </a>
     </p>
   </footer>
