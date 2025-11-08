fd0819358f5b0c736b367749db1b704fbcd6d549
Swayam Shah <swayamshah66@gmail.com>
2025-11-09 00:46:07 +0530

    19

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,11 @@
 import {createApp} from 'vue'
 import App from './App.vue'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faSquareLinkedin } from '@fortawesome/free-brands-svg-icons';
 
-createApp(App).mount('#app')
+library.add(faSquareLinkedin);
+
+const app = createApp(App);
+app.component('FontAwesomeIcon', FontAwesomeIcon);
+app.mount('#app');
