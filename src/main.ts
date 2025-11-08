4e9a870e140e35b9aaffd22d97ad3143bb555173
Swayam Shah <swayamshah66@gmail.com>
2025-11-09 01:15:56 +0530

    30

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,10 @@ import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { faSquareLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
-library.add(faSquareLinkedin, faEnvelope);
+library.add(faSquareLinkedin, faEnvelope, faGithub);
 
 const app = createApp(App);
 app.component('FontAwesomeIcon', FontAwesomeIcon);
